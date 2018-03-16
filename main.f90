83cbb0e8033142ddce091383a437bf4ed2eb5072
cesargarcia2194 <cesargarcia2194@gmail.com>
2018-03-15 23:35:02 -0400

    hacer caras

--- main.f90
+++ main.f90
@@ -3,8 +3,12 @@ program main
   implicit none
   type(contenido_mesh) :: cont_mesh
   cont_mesh = cargar_mesh('sphere.msh')
+
+  call hacer_caras_solidos(cont_mesh)
+
   print*, 'Hola'
   print*, 'Numero de nodos', cont_mesh%nnodos
-  print*, 'Numero de parches triangulares', cont_mesh%nparchest
+ 
   print*, 'numero de solidos', cont_mesh%nsolidos
+  
 end program main
--- mesh.f90
+++ mesh.f90
@@ -11,47 +11,48 @@ MODULE mesh
 
 	
 	TYPE nodo
+		integer, numero_nodo
 		real(KIND=8), DIMENSION(3)::rp !posilbe vector de ubicacion de nodo
 		 
 	END TYPE nodo
-
-  TYPE parchest
-    integer:: id
-    integer, DIMENSION(3):: indice_nodos, indice_ladoscomunes
-    ! los que agregue
-    real,  DIMENSION(3) :: normal, baricentro ! normal: vector normal
-    ! ladoscomunes: vectores directores de los lados comunes
-    ! normal_ladoscomunes: vecor normal a los lados comunes
-    real, DIMENSION(3,3) :: ladoscomunes, normal_ladoscomunes
-    real :: area, pd !! no se que era pd, pero es dot(normal,p1)
-  END TYPE parchest
+	TYPE cara
+    	integer:: id, indice_cara
+    	integer, DIMENSION(3):: indice_nodos, indice_ladoscomunes
+    	! los que agregue
+    	real,  DIMENSION(3) :: normal, baricentro ! normal: vector normal
+    	! ladoscomunes: vectores directores de los lados comunes
+    	! normal_ladoscomunes: vecor normal a los lados comunes
+    	real, DIMENSION(3,3) :: ladoscomunes, normal_ladoscomunes
+    	real :: area, pd !! no se que era pd, pero es dot(normal,p1)
+ 	END TYPE cara
 
 	TYPE lineas 
 		integer:: id
 		integer, DIMENSION(2):: indice_nodos
 	END TYPE lineas
 
-  TYPE solidos
-    integer:: id
-    integer, DIMENSION(4):: indice_nodos
-    INTEGER, DIMENSION(4) :: indices_cara_solido
-  END TYPE solidos
+  	TYPE solidos
+  	  integer:: id
+  	  integer, DIMENSION(4):: indice_nodos
+  	  INTEGER, DIMENSION(4) :: indices_cara_solido
+  	END TYPE solidos
 
-  TYPE cara_solido
-     INTEGER, DIMENSION(3) :: indices_nodo
-     INTEGER, DIMENSION(2) :: indices_solido, indices_bnodo
-     INTEGER :: indece_cara ! -1 if not a boundary
-     REAL (KIND=dp) :: area
-  END TYPE cara_solido
+
+	TYPE cara_solido
+     	INTEGER, DIMENSION(3) :: indices_nodo
+     	INTEGER, DIMENSION(2) :: indices_solido, indices_bnodo
+     	INTEGER :: indece_cara ! -1 if not a boundary
+     	REAL (KIND=8) :: area
+  	END TYPE cara_solido
 
 	TYPE contenido_mesh
 		type(nodo), DIMENSION(:),allocatable :: nodos 
-		type(parchest), DIMENSION(:), allocatable ::parchest
+		type(cara), DIMENSION(:), allocatable ::caras
 		type(lineas), DIMENSION(:), allocatable ::lineas
-    type(solidos), DIMENSION(:), allocatable ::solidos
+    	type(solidos), DIMENSION(:), allocatable ::solidos
 		TYPE(cara_solido), DIMENSION(:), ALLOCATABLE :: caras_solidos
 		integer ::nnodos
-		integer ::nparchest
+		integer ::ncaras
 		integer ::nlineas
 		integer ::nsolidos
 		integer:: ncaras_solido 
@@ -98,7 +99,7 @@ END FUNCTION cmp_triplets
 		type(contenido_mesh)::mesh
 		
 		integer :: fid = 10, iovar, nnodos, numero_nodo, n,&
-         nelementos, numero_elemento, tipo_elemento, nparchest, nlineas, nsolidos, etiqueta
+         nelementos, numero_elemento, tipo_elemento, ncaras, nlineas, nsolidos, etiqueta
         integer, DIMENSION(10) :: datos_elementos !CAMBIA NOMBRE
 
         integer,DIMENSION(:,:),allocatable::tipos_elementos
@@ -165,6 +166,7 @@ END FUNCTION cmp_triplets
     			do n=1, nnodos
     				READ(fid,*,IOSTAT=iovar) numero_nodo, np(1:3)
     				mesh%nodos(n)%rp=np
+    				mesh%nodos(n)%numero_nodo=numero_nodo
     			end do
     			mesh%nnodos=nnodos
     			READ(fid, *, IOSTAT=iovar) linea
@@ -174,11 +176,11 @@ END FUNCTION cmp_triplets
              	end if	
           		
           	elseif (linea =='$Elements') then
-          		mesh%nparchest= COUNT(tipos_elementos(:,1)==2)
+          		mesh%ncaras= COUNT(tipos_elementos(:,1)==2)
           		mesh%nlineas= COUNT(tipos_elementos(:,1)==1)
           		mesh%nsolidos= COUNT(tipos_elementos(:,1)==4)
 
-          		allocate(mesh%parchest(1:mesh%nparchest))
+          		allocate(mesh%caras(1:mesh%ncaras))
 
           		if (mesh%nlineas/=0) then 
           			allocate(mesh%lineas(1:mesh%nlineas))	
@@ -188,7 +190,7 @@ END FUNCTION cmp_triplets
           		end if
           		READ(fid,*,IOSTAT=iovar) nelementos
 
-          		nparchest=0
+          		ncaras=0
           		nlineas=0
           		nsolidos=0
           		
@@ -196,10 +198,10 @@ END FUNCTION cmp_triplets
           		do n=1,nelementos
           			etiqueta=tipos_elementos(n,2)
           			if(tipos_elementos(n,1)==2) then
-          				nparchest=nparchest +1
+          				ncaras=ncaras +1
           				READ(fid,*,IOSTAT=iovar) datos_elementos(1:(etiqueta+ 6))
-          				mesh%parchest(nparchest)%id=datos_elementos(4)
-          				mesh%parchest(nparchest)%indice_nodos(1:3)=datos_elementos((etiqueta+ 4):(etiqueta+ 6))
+          				mesh%caras(ncaras)%id=datos_elementos(4)
+          				mesh%caras(ncaras)%indice_nodos(1:3)=datos_elementos((etiqueta+ 4):(etiqueta+ 6))
           			elseif (tipos_elementos(n,1)==1) then
           				nlineas=nlineas +1
           				READ(fid,*,IOSTAT=iovar) datos_elementos(1:(etiqueta+ 5))
@@ -228,6 +230,67 @@ END FUNCTION cmp_triplets
     	!CERRAR ARCHIVO
 END FUNCTION cargar_mesh
 
+
+
+	function prod_cruz3(v1,v2) RESULT(v1xv2)
+		real(KIND=8),DIMENSION(3),INTENT(IN)::v1
+		real(KIND=8),DIMENSION(3),INTENT(IN)::v2
+		real(KIND=8),DIMENSION(3)::v1xv2
+
+		v1xv2(1)=v1(2)*v2(3)-v2(2)*v1(3)
+		v1xv2(2)=v2(1)*v1(3)-v1(1)*v2(3)
+		v1xv2(3)=v1(1)*v2(2)-v2(1)-v1(2)
+	end function prod_cruz3
+
+	function norma_vector3(v)RESULT(res)
+		real(KIND=8),DIMENSION(3),INTENT(IN)::v
+		real(KIND=8)::res
+		res=SQRT(v(1)**2 +v(2)**2 +v(3)**2)
+	end function norma_vector3
+
+	function hace_vertor3(v1,v2)RESULT(v)
+		real(KIND=8),DIMENSION(3),INTENT(IN)::v1
+		real(KIND=8),DIMENSION(3),INTENT(IN)::v2
+		real(KIND=8),DIMENSION(3)::v
+		v(1)=v2(1)-v1(1)
+		v(2)=v2(2)-v1(2)
+		v(3)=v2(3)-v1(3)
+	end function hace_vertor3
+
+	SUBROUTINE hacer_caras(mesh)
+
+
+		type(contenido_mesh),INTENT(INOUT):: mesh
+		integer:: n
+
+		real(KIND=8),DIMENSION(3)::e1, e2, e3, normal, normalu
+
+
+		DO n=1,mesh%ncaras
+
+			e1=hace_vertor3(mesh%nodos%(mesh%caras(n)%indice_nodos(1))%rp, mesh%nodos%(mesh%caras(n)%indice_nodos(2))%rp)
+			e2=hace_vertor3(mesh%nodos%(mesh%caras(n)%indice_nodos(2))%rp, mesh%nodos%(mesh%caras(n)%indice_nodos(3))%rp)
+			e3=hace_vertor3(mesh%nodos%(mesh%caras(n)%indice_nodos(3))%rp, mesh%nodos%(mesh%caras(n)%indice_nodos(1))%rp)
+
+			normal = prod_cruz3(e1,e2)
+			area = norma_vector3(normal)
+
+			e1 = e1/norma_vector3(e1)
+			e2 = e2/norma_vector3(e2)
+			e3 = e3/norma_vector3(e3) 
+
+			normalu = normal/norma_vector3(normal)	
+
+			mesh%caras(n)%normal(:)=normalu(:)
+
+			mesh%caras(n)%ladoscomunes(1,:)=e1(:)
+			mesh%caras(n)%ladoscomunes(2,:)=e2(:)
+			mesh%caras(n)%ladoscomunes(3,:)=e3(:)
+
+			mesh%caras(n)%area=area
+
+		END DO
+END SUBROUTINE hacer_caras
 
 
 !--------------------------------------------------------------------
@@ -270,7 +333,7 @@ SUBROUTINE hacer_caras_solidos(mesh)
                 ! If an edge is shared by more than two faces, only two connections
                 ! are recorded.
                 tmpcaras(l)%indices_solido(2) = n
-                tmpcaras(l)%indices_bnodo(2) = mesh%solidos(n)%indices_nodo(indexrot4(m+3))
+                tmpcaras(l)%indices_bnodo(2) = mesh%solidos(n)%indice_nodos(indexrot4(m+3))
 
                 ! Add this face index to tetrahedra's face list.
                 mesh%solidos(n)%indices_cara_solido(m) = l
@@ -286,7 +349,7 @@ SUBROUTINE hacer_caras_solidos(mesh)
 
              ! Add this face index to edge's face list as the first one.
              tmpcaras(ccara)%indices_solido(1) = n
-             tmpcaras(ccara)%indices_bnodo(1) = mesh%solidos(n)%indices_nodo(indexrot4(m+3))
+             tmpcaras(ccara)%indices_bnodo(1) = mesh%solidos(n)%indice_nodos(indexrot4(m+3))
 
              ! Add this face index to tetrahedra's face list.
              mesh%solidos(n)%indices_cara_solido(m) = ccara
@@ -310,9 +373,9 @@ SUBROUTINE hacer_caras_solidos(mesh)
     ! Connect boundary solid faces to surface faces.
     DO n=1,mesh%ncaras_solido
        IF(mesh%caras_solidos(n)%indices_solido(1)==-1 .OR.&
-            mesh%solid_faces(n)%indices_solido(2)==-1) THEN
+            mesh%caras_solidos(n)%indices_solido(2)==-1) THEN
           DO m=1,mesh%ncaras
-             IF(cmp_triplets(mesh%caras_solidos(n)%indices_nodo, mesh%parchest(m)%indice_nodos)) THEN
+             IF(cmp_triplets(mesh%caras_solidos(n)%indices_nodo, mesh%caras(m)%indice_nodos)) THEN
                 mesh%caras_solidos(n)%indece_cara = m
                 EXIT
              END IF
