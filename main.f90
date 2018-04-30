bad655aa637d45c652b29e516d6cfb145cb69e3e
vicmans <vsanchezl94@gmail.com>
2018-04-30 10:13:14 -0400

    cambios q no se subieron

--- main.f90
+++ main.f90
@@ -14,9 +14,9 @@ program main
   call printconsole('Hola')
   print *, cont_mesh%lados
 
-  ! call inicializar_MLFMA(cont_mesh%centro, cont_mesh%nlados, cont_mesh%lados(:)%indices_nodo, &
-  !  ont_mesh%lados(:)%bnodes, cont_mesh%lados(:)%longitud, t_area, t_baric, t_baric_sub, p_coord, t_p, e_p, num_p,&
-  !        num_t, t_normal)
+  call inicializar_MLFMA(cont_mesh%centro, cont_mesh%nlados, cont_mesh%lados(:)%indices_nodo, &
+    ont_mesh%lados(:)%bnodes, cont_mesh%lados(:)%longitud, t_area, t_baric, t_baric_sub, p_coord, t_p, e_p, num_p,&
+          num_t, t_normal)
 
 
 end program main
