ff048845a3fca7839f144a1875b56d4b477c2e7c
vicmans <vsanchezl94@gmail.com>
2018-05-06 13:41:47 -0400

    Prueba3: pasar de mesh_container a formato de la tesis vieja

--- tests/prueba3.f90
+++ tests/prueba3.f90
@@ -11,16 +11,17 @@ program prueba3
   integer :: i
   ! variables para los modulos
   integer ( kind = il ), allocatable, dimension(:,:) :: t_p
-    integer ( kind = il ), allocatable, dimension(:,:) :: e_p
-    integer ( kind = il ), allocatable, dimension(:,:) :: e_t
-    integer ( kind = il ), allocatable, dimension(:,:) :: e_po
-    real ( kind = dp ), allocatable, dimension(:,:) :: p_coord
-    real (kind = dp ), allocatable, dimension(:) :: e_long
-    real ( kind = dp ), allocatable, dimension(:) :: t_area
-    real ( kind= dp ), allocatable, dimension(:,:) :: t_normal
-    real ( kind = dp ), allocatable, dimension(:,:) :: e_centro
-    real ( kind = dp ), allocatable, dimension(:,:) :: t_baric
-    real ( kind = dp ), allocatable, dimension(:,:,:) :: t_baric_sub
+  integer ( kind = il ), allocatable, dimension(:,:) :: e_p
+  integer ( kind = il ), allocatable, dimension(:,:) :: e_t
+  integer ( kind = il ), allocatable, dimension(:,:) :: e_po
+  real ( kind = dp ), allocatable, dimension(:,:) :: p_coord
+  real (kind = dp ), allocatable, dimension(:) :: e_long
+  real ( kind = dp ), allocatable, dimension(:) :: t_area
+  real ( kind= dp ), allocatable, dimension(:,:) :: t_normal
+  real ( kind = dp ), allocatable, dimension(:,:) :: e_centro
+  real ( kind = dp ), allocatable, dimension(:,:) :: t_baric
+  real ( kind = dp ), allocatable, dimension(:,:,:) :: t_baric_sub
+  integer ( kind = il ) :: num_p, num_t
 
 
   print *, 'Nombre del archivo'
@@ -39,21 +40,33 @@ program prueba3
   allocate(p_coord(3,meshs%nnodes))
   do i=1,meshs%nnodes
     p_coord(:,i) = meshs%nodes(i)%p
-    print*, meshs%edges(i)%bnode_indices
+    !print*, meshs%edges(i)%bnode_indices
   enddo
   allocate(t_p(3,size(meshs%faces)))
   do i=1,meshs%nfaces
-    !t_area = meshs%faces(i)%area
-    !t_baric(:,i) = meshs%faces(i)%cp
+    t_area = meshs%faces(i)%area
+    t_baric(:,i) = meshs%faces(i)%cp
     t_p(:,i) = meshs%faces(i)%node_indices
   enddo
+  allocate(e_p(2,size(meshs%edges)))
+  allocate(e_long(meshs%nedges))
+  allocate(e_po(2,size(meshs%edges)))
+  allocate(e_t(2,size(meshs%edges)))
+  do i=1,meshs%nedges
+    e_long = meshs%edges(i)%length
+    e_po(:,i) = meshs%edges(i)%bnode_indices
+    e_p(:,i) = meshs%edges(i)%node_indices
+    e_t(:,i) = meshs%edges(i)%face_indices
+  enddo
+  !print*, shape(t_p)
+  num_p = meshs%nnodes
+  num_t = meshs%nfaces
   
-  call hallar_geometria_rwg(p_coord,t_p,meshs%nnodes,meshs%nedges,t_area,t_baric,t_baric_sub)
+  call hallar_geometria_rwg(p_coord,t_p,meshs%nnodes,meshs%nfaces,t_area,t_baric,t_baric_sub)
 
-
-!  call inicializar_MLFMA(meshs%centers, meshs%nedges, meshs%edges(:)%node_indices, &
-!    meshs%edges(:)%bnode_indices, meshs%edges(:)%length, t_area, t_baric, t_baric_sub, p_coord, t_p, e_p, num_p,&
-!          num_t, t_normal)
+  call inicializar_MLFMA(meshs%centers, meshs%nedges, e_t, &
+    e_po, e_long, t_area, t_baric, t_baric_sub, p_coord, t_p, e_p, num_p,&
+          num_t, t_normal)
 
 
 end program prueba3
